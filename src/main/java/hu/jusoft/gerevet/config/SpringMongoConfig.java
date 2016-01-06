aa1b9a4bcb593c345c52f22e8b492a31ee83ffd2
ReginaObengo <regina@obengo.com>
2016-01-06 17:55:51 +0100

    fix credentials

--- src/main/java/hu/jusoft/gerevet/config/SpringMongoConfig.java
+++ src/main/java/hu/jusoft/gerevet/config/SpringMongoConfig.java
@@ -2,10 +2,13 @@ package hu.jusoft.gerevet.config;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.WriteConcern;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
 /**
@@ -18,6 +21,12 @@ import org.springframework.data.mongodb.gridfs.GridFsTemplate;
     @Value("${spring.data.mongodb.host}")
     private String mongoUrl;
 
+    @Value("${spring.data.mongodb.username}")
+    private String  userName;
+
+    @Value("${spring.data.mongodb.password}")
+    private String  password;
+
     @Bean
     public GridFsTemplate gridFsTemplate() throws Exception {
         return new GridFsTemplate(mongoDbFactory(), mappingMongoConverter());
@@ -26,6 +35,18 @@ import org.springframework.data.mongodb.gridfs.GridFsTemplate;
     @Override
     protected String getDatabaseName() {
         return "gerevet";
+    }
+
+    @Override
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception
+    {
+        final UserCredentials userCredentials = new UserCredentials(this.userName, this.password);
+
+        final MongoTemplate mongoTemplate = new MongoTemplate(mongo(), getDatabaseName(), userCredentials);
+        mongoTemplate.setWriteConcern(WriteConcern.SAFE);
+
+        return mongoTemplate;
     }
 
     @Override
